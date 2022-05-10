54336348ba77e632eb98665822db13ef84f5cfb9
heesung33 <chaeheesung33@gmail.com>
2022-05-10 17:04:07 +0900

    🔫 자바 예외처리

---
+++ 자바 코드/src/day_0510/no1/NullPointerExceptionExample.java
@@ -0,0 +1,8 @@
+package day_0510.no1;
+
+public class NullPointerExceptionExample {
+    public static void main(String[] args) {
+        String data = null;
+        System.out.println(data.toString());
+    }
+}
