fd21d46430ddf013d84e6b7d0115409a8d456307
heesung33 <chaeheesung33@gmail.com>
2022-05-23 11:10:36 +0900

    🔫 MainTest

---
+++ 자바 코드/src/day_0517/no3/MainTest.java
@@ -0,0 +1,20 @@
+package day_0517.no3;
+
+public class MainTest {
+    public static void main(String[] args) {
+        long time1 = System.currentTimeMillis();
+        long t1 = System.nanoTime();
+
+        int sum = 0;
+        for(int i = 0; i<= 1000000; i++) {
+            sum += i;
+        }
+
+        long time2 = System.currentTimeMillis();
+        long t2 = System.nanoTime();
+
+        System.out.println("1~1000000" + sum);
+        System.out.println("계산에" + (time2 - time1) + "밀리초가 소요되었습니다.");
+        System.out.println("계산에 + " + (t2 - t1) + "나노초가 소요되었습니다");
+    }
+}
