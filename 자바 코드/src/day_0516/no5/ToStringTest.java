5f2809a7b971ae48d0187d5ba9faa1d5d4f1b8b0
heesung33 <chaeheesung33@gmail.com>
2022-05-23 11:09:34 +0900

    🔫 ToStringTest

---
+++ 자바 코드/src/day_0516/no5/ToStringTest.java
@@ -0,0 +1,14 @@
+package day_0516.no5;
+
+public class ToStringTest {
+
+    public static void main(String[] args) {
+        Object obj = new Object();
+        System.out.println(obj.toString());
+
+        String name = new String("이름");
+        System.out.println(name.toString());
+
+        System.out.println(name);
+    }
+}
