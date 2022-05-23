eb3364b82f096d789ab543bb8ca774195bac0462
heesung33 <chaeheesung33@gmail.com>
2022-05-23 11:10:14 +0900

    🔫 Counter

---
+++ 자바 코드/src/day_0517/no2/Counter.java
@@ -0,0 +1,15 @@
+package day_0517.no2;
+
+public class Counter {
+
+    private int no;
+
+    public Counter(int no) {
+        this.no = no;
+    }
+
+    @Override
+    protected void finalize() throws Throwable{
+        System.out.println(no + "번 객체의 finalize()가 실행됨");
+    }
+}
