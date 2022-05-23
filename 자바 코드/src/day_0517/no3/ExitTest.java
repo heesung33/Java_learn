6d307e24b172b390d2eaca5f9b929c60220fd26a
heesung33 <chaeheesung33@gmail.com>
2022-05-23 11:10:29 +0900

    🔫 ExitTest

---
+++ 자바 코드/src/day_0517/no3/ExitTest.java
@@ -0,0 +1,17 @@
+package day_0517.no3;
+
+public class ExitTest {
+
+    public static void main(String[] args) {
+        for(int i = 0; i<10; i++) {
+            System.out.println(i);
+        }
+
+        System.exit(0);
+
+        for(int i = 0; i<10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
