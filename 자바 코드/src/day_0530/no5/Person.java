e3c8b161ecbd12db249a0344d084fcbfe85dbe96
heesung33 <chaeheesung33@gmail.com>
2022-05-31 15:05:59 +0900

    🔫 Person

---
+++ 자바 코드/src/day_0530/no5/Person.java
@@ -0,0 +1,20 @@
+package day_0530.no5;
+
+public class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
