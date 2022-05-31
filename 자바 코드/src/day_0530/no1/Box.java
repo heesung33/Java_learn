c810d1ad01bb0e6a0f20b575357942ce7ca0faa8
heesung33 <chaeheesung33@gmail.com>
2022-05-31 15:06:54 +0900

    🔫 Box

---
+++ 자바 코드/src/day_0530/no1/Box.java
@@ -0,0 +1,11 @@
+package day_0530.no1;
+
+public class Box {
+    private Object obj;
+    public Object getObj() {
+        return obj;
+    }
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+}
