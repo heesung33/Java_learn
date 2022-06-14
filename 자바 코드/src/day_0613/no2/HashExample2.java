5029bd188eae52f9332ef1ad2d1d0f5d9d595de7
heesung33 <chaeheesung33@gmail.com>
2022-06-14 13:42:49 +0900

    🔫 HashExample2

---
+++ 자바 코드/src/day_0613/no2/HashExample2.java
@@ -0,0 +1,17 @@
+package day_0613.no2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashExample2 {
+
+    public static void main(String[] args) {
+        Set<Member> set = new HashSet<Member>();
+
+        set.add(new Member("홍길동", 30));
+        set.add(new Member("홍길동", 30));
+
+        System.out.println("총 객체수: " + set.size());
+    }
+
+}
